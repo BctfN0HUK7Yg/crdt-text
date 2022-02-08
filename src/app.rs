746c46ac13493541b81d9be8bb533955f5a91d52
BctfN0HUK7Yg <oksimets@yandex.ru>
2022-02-08 19:58:05 +0300

    123

--- src/app.rs
+++ src/app.rs
@@ -1,36 +1,38 @@
 use eframe::{egui, epi};
+use yrs::*;
 
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[cfg_attr(feature = "persistence", derive(serde::Deserialize, serde::Serialize))]
 #[cfg_attr(feature = "persistence", serde(default))] // if we add new fields, give them default values when deserializing old state
-pub struct TemplateApp {
+pub struct CrdtTextApp {
     // Example stuff:
     label: String,
-
+    doc:Doc,
     // this how you opt-out of serialization of a member
     #[cfg_attr(feature = "persistence", serde(skip))]
     value: f32,
 }
 
-impl Default for TemplateApp {
+impl Default for CrdtTextApp {
     fn default() -> Self {
         Self {
             // Example stuff:
             label: "Hello World!".to_owned(),
             value: 2.7,
+            doc:Doc::new(),
         }
     }
 }
 
-impl epi::App for TemplateApp {
+impl epi::App for CrdtTextApp {
     fn name(&self) -> &str {
-        "eframe template"
+        "CRDT-text"
     }
 
     /// Called once before the first frame.
     fn setup(
         &mut self,
-        _ctx: &egui::CtxRef,
+        _ctx: &egui::Context,
         _frame: &epi::Frame,
         _storage: Option<&dyn epi::Storage>,
     ) {
@@ -40,6 +42,30 @@ impl epi::App for TemplateApp {
         if let Some(storage) = _storage {
             *self = epi::get_value(storage, epi::APP_KEY).unwrap_or_default()
         }
+
+        let doc = Doc::new();
+        let mut txn = doc.transact(); // all Yrs operations happen in scope of a transaction
+        let text = txn.get_text("name");
+        // append text to our collaborative document
+        text.push(&mut txn, "hello world");
+
+        // simulate update with remote peer
+        let remote_doc = Doc::new();
+        let mut remote_txn = remote_doc.transact();
+        let remote_text = remote_txn.get_text("name");
+
+        // in order to exchange data with other documents
+        // we first need to create a state vector
+        let state_vector = remote_doc.get_state_vector(&remote_txn);
+
+        // now compute a differential update based on remote document's state vector
+        let update = doc.encode_delta_as_update_v1(&txn, &state_vector);
+
+        // both update and state vector are serializable, we can pass them over the wire
+        // now apply update to a remote document
+        remote_doc.apply_update_v1(&mut remote_txn, update.as_slice());
+
+        println!("{}", remote_text.to_string(&remote_txn));
     }
 
     /// Called by the frame work to save state before shutdown.
@@ -51,7 +77,7 @@ impl epi::App for TemplateApp {
 
     /// Called each time the UI needs repainting, which may be many times per second.
     /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
-    fn update(&mut self, ctx: &egui::CtxRef, frame: &epi::Frame) {
+    fn update(&mut self, ctx: &egui::Context, frame: &epi::Frame) {
         let Self { label, value } = self;
 
         // Examples of how to create different panels and windows.
